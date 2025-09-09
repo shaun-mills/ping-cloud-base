a9f7a6434b4dff74e288cf9f80759e628329b51d
Shaun Mills <shaunmills@pingidentity.com>
2025-09-09 16:18:12 -0700

    [skip pipeline] refactor(argocd-ingress): self-service annotation patch [skip-pipeline]

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -1447,6 +1447,9 @@ for ENV_OR_BRANCH in ${SUPPORTED_ENVIRONMENT_TYPES}; do
     name: argocd-ingress
   patch: |-
     - op: replace
+      path: /metadata/annotations/selfservice.metadata.pingidentity.com~1displayURL
+      value: "https://argocd.${DNS_ZONE}/"
+    - op: replace
       path: /spec/tls/0/hosts/0
       value: argocd.${DNS_ZONE}
     - op: replace
