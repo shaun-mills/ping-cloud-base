2c871e6804fc3b3bd4ae482cf412a3f4d36e8122
Shaun Mills <shaunmills@pingidentity.com>
2025-12-15 10:42:05 -0800

    style(chub): comments [skip pipeline]

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -743,12 +743,14 @@ organize_code_for_csr() {
             done
             # delete all prod-values.yaml files
             find "${app_target_dir}" -type f -name "prod-values.yaml" -exec rm -f {} +
+          # If no chub-values.yaml files exist, use prod-values.yaml files
           elif [ -n "${prod_values_files}" ]; then
             echo "No chub-values.yaml files found for app ${app_name}, using prod-values.yaml"
             for prod_values_file in ${prod_values_files}; do
               yq -i ". *= load(\"${prod_values_file}\")" "${prod_values_file//prod-/}"
               rm -f $prod_values_file
             done
+          # If neither chub-values.yaml nor prod-values.yaml files exist, use base values.yaml
           else
             echo "No chub-values.yaml or prod-values.yaml files found for app ${app_name}, using base values.yaml"
           fi
